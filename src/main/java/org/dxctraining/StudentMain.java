8615fefdfc8f5149a2e86cfe7557521564098aff
roopsai <roopsaichalla1@gmail.com>
2020-08-07 14:36:44 +0530

    first commit

---
+++ src/main/java/org/dxctraining/StudentMain.java
@@ -0,0 +1,31 @@
+package org.dxctraining;
+
+import java.util.*;
+
+public class StudentMain {
+	private Set<Student> set = new HashSet<>();
+
+	public static void main(String[] args) {
+		StudentMain run = new StudentMain();
+		run.runApp();
+	}
+
+	public void runApp() {
+		set.add(new Student("a1", 23));
+		set.add(new Student("b2", 21));
+		set.add(new Student("c3", 20));
+		set.add(new Student("d4", 22));
+		List<Student> list = changedlist(set);
+	}
+
+	public List<Student> changedlist(Set<Student> set) {
+		List<Student> list = new ArrayList<>();
+		for (Student student : set) {
+			if (student.getAge() > 21) {
+				list.add(student);
+			}
+		}
+		return list;
+	}
+
+}
